ec404ac7672cb95830822bca2d68552d0e9b1b6c
renzymigz <itsrealflorence@gmail.com>
2026-03-07 10:21:20 +0800

    feat(entity): add AuthProvider, Role, and UserStatus enums

---
+++ backend/src/main/java/edu/cit/migallos/techlend/enums/Role.java
@@ -0,0 +1,8 @@
+package edu.cit.migallos.techlend.enums;
+
+public enum Role {
+    STUDENT,
+    INSTRUCTOR,
+    LAB_STAFF,
+    ADMIN
+}
---
+++ backend/src/main/java/edu/cit/migallos/techlend/enums/UserStatus.java
@@ -0,0 +1,6 @@
+package edu.cit.migallos.techlend.enums;
+
+public enum UserStatus {
+    ACTIVE,
+    SUSPENDED
+}
