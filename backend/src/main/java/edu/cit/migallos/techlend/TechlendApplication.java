bdea8f404a18fa8548c273b72398a28be4187b4a
renzymigz <itsrealflorence@gmail.com>
2026-03-07 08:34:43 +0800

    feat(init): base project setup for vite and spring boot

---
+++ backend/src/main/java/edu/cit/migallos/techlend/TechlendApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.migallos.techlend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechlendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechlendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/cit/migallos/techlend/TechlendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.migallos.techlend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechlendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
